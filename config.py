aa9544ffa1f64699db0e6a283904c5e2afa62272
Rom Zamora <90288942+Mr-Zamora@users.noreply.github.com>
2025-06-09 08:31:14 +1000

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@
 # This file should be added to .gitignore to prevent committing sensitive information
 
 # Gemini API key
-GEMINI_API_KEY = "AIzaSyCque0JOqjiqK6vl1bWDQRiF8QKri05WhU"
+GEMINI_API_KEY = "#"
